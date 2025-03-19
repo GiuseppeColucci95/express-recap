baf4d7e065f72d0e04ceef9ad6b795e78f085828
GiuseppeColucci95 <colucci539@gmail.com>
2025-03-19 10:39:10 +0100

    Implement CRUD routes for songs in the server

--- server.js
+++ server.js
@@ -7,7 +7,58 @@ app.listen(port, () => {
   console.log(`Server start running on port http://localhost:${port}`);
 });
 
-//routes
+//MIDDLEWARES
+
+//body parser
+app.use(express.json());
+
+//ROUTES
+
+//home
 app.get('/', (req, res) => {
   res.send("Hello World!");
+});
+
+//index
+app.get('/api/v1/songs', (req, res) => {
+  res.send("Return all songs");
+});
+
+//show
+app.get('/api/v1/songs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+
+  res.send(`Return song with id: ${id}`);
+});
+
+//store
+app.post('/api/v1/songs', (req, res) => {
+  console.log(req.body);
+
+  res.send("Create a new song");
+});
+
+//update
+app.put('/api/v1/songs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+
+  res.send(`Update song with id: ${id}`);
+});
+
+//modify
+app.patch('/api/v1/songs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+
+  res.send(`Modify song with id: ${id}`);
+});
+
+//destroy
+app.delete('/api/v1/songs/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(req.params);
+
+  res.send(`Delete song with id: ${id}`);
 });
